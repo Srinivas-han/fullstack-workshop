97f87033cd353a92405b0fa51955d1a3eb207c9f
Srinivas-han <madhuhanumanthu123@gmail.com>
2025-12-23 13:37:18 +0530

    created a new repository

---
+++ script.js
@@ -0,0 +1,13 @@
+ console.log('Hello World!!');
+console.warn('This is a warning');
+console.error('This is an error');
+console.info('This is info');
+console.log('Name:', 'John', 'Age:', 25);
+console.log({ name: 'John', age: 25 });
+console.table([
+    { name: 'John', age: 25 },
+    { name: 'Jane', age: 30 }
+]);
+alert('Welcome to JavaScript!');
+let name = prompt('What is your name?');
+console.log('Hello, ' + name);
